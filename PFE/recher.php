468a7d459254c4c9fd481e58d9065ca71e817724
Khaoula ch <110127385+khaoula-ch-ch@users.noreply.github.com>
2023-06-02 19:33:15 +0100

    Update recher.php

--- PFE/recher.php
+++ PFE/recher.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="fran">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8" />
-    <title>Courrier Sortants</title>
-    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet"  href="styleaccueil.css">
     <link rel="stylesheet"  href="styleformulaire.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
- </head>
+	<title>Rechercher un courrier</title>
+</head>
 <body translate="no" >
     <br>
 <div class="naVbar">
@@ -27,95 +26,85 @@
               </li>
            </ul>
         </naV>
-    </div>
+    </div></body>
 <body>
     <div class="formulaire">
         <div class="barre"></div><br>
         <form method="post">
+        
+            <table class="table">
+           
             <h2> Rechercher Un courrier</h2>
-            <form method="post">
+            <center><input type="text" name="search">
+<input type="submit" name="submit"></center>
+     
+      <thead class="thead-dark">
+    <tr>
+    
+      <th scope="col">Ref</th>
+      <th scope="col">N.courrier</th>
+      <th scope="col">confidentiel</th>
+      <th scope="col">Date.cour</th>
+      <th scope="col">Envoyer</th>
+      <th scope="col">Dis</th>
+      <th scope="col">Sous.dis</th>
+      <th scope="col">Date.tra</th>
+      <th scope="col">Rema</th>
+      <th scope="col">Objet</th>
+      <th scope="col">Naturedoc</th>
+      <th scope="col">Fichier</th>
+    </tr>
+    <?php
 
-<center><input type="text" name="search" >
-<input type="submit" name="submit" value="rechercher"></center>
-<?php
+$conn = mysqli_connect('localhost','root','','courrier') or die('connection failed');
+session_start();
 
-$con = new PDO("mysql:host=localhost;dbname=courrier",'root','');
 
 if (isset($_POST["submit"])) {
 	$str = $_POST["search"];
-	$sth = $con->prepare("SELECT * FROM `courrierarrive` WHERE envoyerpar = '$str' OR ref='$str' OR distination='$str' OR sousdistination='$str' OR objet='$str'");
-
-	$sth->setFetchMode(PDO:: FETCH_OBJ);
-	$sth -> execute();
-
-	if($row = $sth->fetch())
-	{
-		?>
-		<br><br><br>
-                          <table class="table">
-           
-      
-     
-           <thead class="thead-dark">
-         <tr>
-         
-           <th scope="col">Ref</th>
-           <th scope="col">N.courrier</th>
-           <th scope="col">confidentiel</th>
-           <th scope="col">Date.cour</th>
-           <th scope="col">Envoyer</th>
-           <th scope="col">Dis</th>
-           <th scope="col">Sous.dis</th>
-           <th scope="col">Date.tra</th>
-           <th scope="col">Rema</th>
-           <th scope="col">Objet</th>
-           <th scope="col">Naturedoc</th>
-           <th scope="col">Fichier</th>
-         </tr>
-         <tr><td><?= $row->ref?></td>
-                <td><?= $row->naturecour?></td>
-                <td><?= $row->confidentiel ?> </td>
-                <td ><?= $row->datecourr ?></td>
-                <td ><?= $row->envoyerpar ?></td>
-                <td><?= $row->distination ?></td>
-                <td ><?= $row->sousdistination ?></td>
-                <td ><?= $row->datetraite?></td>
-                <td ><?= $row->remarque ?></td>
-                <td ><?= $row->objet ?></td>
-                <td ><?= $row->naturedoc ?></td>
-                <td><a href="pdf.php?id=<?= $row->id ?>"><img src="voir.png" style=width:40px;></a></td>
-      
-		</table>
-<?php 
-	}
-		
-		
-		else{
-			echo "Name Does not exist";
-		}
 
 
+$sql = "SELECT * FROM `courrierarrive` WHERE envoyerpar = '$str' OR ref='$str' OR distination='$str' OR sousdistination='$str' OR objet='$str' ";
+
+if ($result = mysqli_query($conn, $sql)) {
+  // Fetch one and one row
+    while ($row = mysqli_fetch_row($result)) {?>
+      <tr><td><?php echo $row[1] ; ?></td>
+      <td> <?php echo $row[2] ; ?></td>
+      <td><?php echo $row[3] ; ?></td>
+      <td><?php echo $row[4] ; ?></td>
+      <td><?php echo $row[5] ; ?></td>
+      <td><?php echo $row[6] ; ?></td>
+      <td><?php echo $row[7] ; ?></td>
+      <td><?php echo $row[8] ; ?></td>
+      <td><?php echo $row[9] ; ?></td>
+      <td><?php echo $row[10] ; ?></td>
+      <td><?php echo $row[11] ; ?></td>
+      <td><a href="pdf.php?id=<?php echo $row[0] ; ?>"><img src="voir.png" style=width:40px;></a></td>
+      </tr>
+      <?php
+        }
+      }
+
+    mysqli_free_result($result);
+  
+  
 }
 
-?>
+     
+  ?>
+
+
+
+
 	
 </form>
-      
 
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-    </body>
+</body>
 </html>
+
+
+
 
            
 
